b198ae92689d39bd58d0d7bc13f84b12d527cb81
abdelrhman <abdelrhman>
2025-11-19 19:27:10 +0200

    inti

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const html = fs.readFileSync('index.html', 'utf8').toString();
+
+// Logger middleware - logs all requests
+app.use(morgan('combined'));
+
+// Custom logging middleware for additional details
+app.use((req, res, next) => {
+  const timestamp = new Date().toISOString();
+  console.log(`[${timestamp}] ${req.method} ${req.path} - IP: ${req.ip}`);
+  next();
+});
+
+
+app.head('/', (req, res) => {
+  console.log('HEAD request received - Redirecting to Yu-Gi-Oh API');
+  res.header({
+    'Access-Control-Allow-Methods': 'POST',
+  });
+
+});
+
+app.get('/', (req, res) => {
+
+    res.header({
+    'Content-Type': 'text/html'
+  });
+  res.send(html);
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
